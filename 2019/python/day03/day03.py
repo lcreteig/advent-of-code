225bf30b35785784654e3d4c57b0060407eae57a
Leon Reteig <leonreteig@gmail.com>
2021-01-26 10:23:22 +0100

    Add rest of day 3

--- 2019/python/day03/day03.py
+++ 2019/python/day03/day03.py
@@ -2,8 +2,7 @@
 
 
 def get_coords(wire):
-    # Make this a list of dicts!
-    """Get all (x,y) coords where line starts, ends, or bends"""
+    """Returns list of (x,y) coords where line starts, ends, or bends"""
     x, y = [0], [0]
     for idx, point in enumerate(wire):
         dir = point[0]  # which direction to move
@@ -24,10 +23,11 @@ def get_coords(wire):
 
 
 def get_intersections(coords1, coords2):
-    """Return coordinates of intersections of two wires"""
+    """Returns list of (x,y) coordinates of intersections of two wires"""
     intersections = []
-    for p1 in range(len(coords1) - 1):
+    for p1 in range(len(coords1) - 1):  # for each wire segment
         for p2 in range(len(coords2) - 1):
+            # make a bit more expressive, for easier calculation
             line1_start = {"x": coords1[p1][0], "y": coords1[p1][1]}
             line1_end = {"x": coords1[p1 + 1][0], "y": coords1[p1 + 1][1]}
             line2_start = {"x": coords2[p2][0], "y": coords2[p2][1]}
@@ -41,12 +41,20 @@ def get_intersections(coords1, coords2):
                     min(line1_start["x"], line1_end["x"])
                     <= line2_start["x"]
                     <= max(line1_start["x"], line1_end["x"])
-                ) and (  # and if line1s y-coord is inbetween the two y coords of line2
+                ) and (  # and if line1's y-coord is inbetween the two y coords of line2
                     min(line2_start["y"], line2_end["y"])
                     <= line1_start["y"]
                     <= max(line2_start["y"], line2_end["y"])
                 ):
-                    intersections.append((line1_start["y"], line2_start["x"]))
+                    intersections.append(
+                        {  # the intersection is the y-coord of the horizontal line,
+                            # and the x-coord of the vertical line
+                            "coords": (line2_start["x"], line1_start["y"]),
+                            # which segment the intersection is in
+                            "segments_wire1": p1,
+                            "segments_wire2": p2,
+                        }
+                    )  # ditto for wire 2
             else:  # otherwise it's vertical
                 # line1 will intersect line2 if its x_coord is
                 # inbetween the two x-coords of line2,
@@ -59,25 +67,65 @@ def get_intersections(coords1, coords2):
                     <= line2_start["y"]
                     <= max(line1_start["y"], line1_end["y"])
                 ):
-                    intersections.append((line1_start["x"], line2_start["y"]))
+                    intersections.append(
+                        {
+                            "coords": (line1_start["x"], line2_start["y"]),
+                            "segments_wire1": p1,
+                            "segments_wire2": p2,
+                        }
+                    )
     return intersections
 
 
 def manhattan_dist(p, q):
-    """Calculate Manhattan distance of a coordinate to zero"""
+    """Return Manhattan distance between two points (x1,y1) and (x2,y2)"""
     return abs(p[0] - q[0]) + abs(p[1] - q[1])
+
+
+def get_steps(coords, n_segments, intersection):
+    """Number of steps on the grid until an intersection is reached"""
+    steps = 0
+    for i in range(n_segments):  # for all whole segments
+        steps += abs(
+            sum(map(lambda x, y: x - y, coords[i], coords[i + 1]))
+        )  # calculate how far we moved from one coordinate to the next
+        # then add the steps from the last coordinate to the intersection
+    steps += abs(sum(map(lambda x, y: x - y, coords[n_segments], intersection)))
+    return steps
 
 
 def answer_part1(wire1, wire2):
     coords1 = get_coords(wire1)
     coords2 = get_coords(wire2)
     intersections = get_intersections(coords1, coords2)
-    return min([manhattan_dist((0, 0), p) for p in intersections if p != (0, 0)])
+    # minimum manhattan distance for all intersections (disregarding the origin)
+    return min(
+        [
+            manhattan_dist((0, 0), p["coords"])
+            for p in intersections
+            if p["coords"] != (0, 0)
+        ]
+    )
+
+
+def answer_part2(wire1, wire2):
+    coords1 = get_coords(wire1)
+    coords2 = get_coords(wire2)
+    intersections = get_intersections(coords1, coords2)
+    # minimum sum of steps to all intersections (disregarding the origin)
+    return min(
+        [
+            get_steps(coords1, p["segments_wire1"], p["coords"])
+            + get_steps(coords2, p["segments_wire2"], p["coords"])
+            for p in intersections
+            if p["coords"] != (0, 0)
+        ]
+    )
 
 
 if __name__ == "__main__":
     with open("input_day03") as f:
         wire1 = f.readline().strip().split(",")
         wire2 = f.readline().strip().split(",")
-    # Part 1
     print("Part 1:", answer_part1(wire1, wire2))
+    print("Part 2:", answer_part2(wire1, wire2))
--- 2019/python/day03/test_day03.py
+++ 2019/python/day03/test_day03.py
@@ -1,34 +1,29 @@
 import pytest
-from day03 import answer_part1
+from day03 import answer_part1, answer_part2
 
 
-@pytest.mark.parametrize(
-    "wire1, wire2, distance",
-    [
-        (["R8", "U5", "L5", "D3"], ["U7", "R6", "D4", "L4"], 6),
-        (
-            ["R75", "D30", "R83", "U83", "L12", "D49", "R71", "U7", "L72"],
-            ["U62", "R66", "U55", "R34", "D71", "R55", "D58", "R83"],
-            159,
-        ),
-        (
-            [
-                "R98",
-                "U47",
-                "R26",
-                "D63",
-                "R33",
-                "U87",
-                "L62",
-                "D20",
-                "R33",
-                "U53",
-                "R51",
-            ],
-            ["U98", "R91", "D20", "R16", "D67", "R40", "U7", "R15", "U6", "R7"],
-            135,
-        ),
-    ],
-)
-def test_answer_part1(wire1, wire2, distance):
+testdata = [
+    (["R8", "U5", "L5", "D3"], ["U7", "R6", "D4", "L4"], 6, 30),
+    (
+        ["R75", "D30", "R83", "U83", "L12", "D49", "R71", "U7", "L72"],
+        ["U62", "R66", "U55", "R34", "D71", "R55", "D58", "R83"],
+        159,
+        610,
+    ),
+    (
+        ["R98", "U47", "R26", "D63", "R33", "U87", "L62", "D20", "R33", "U53", "R51"],
+        ["U98", "R91", "D20", "R16", "D67", "R40", "U7", "R15", "U6", "R7"],
+        135,
+        410,
+    ),
+]
+
+
+@pytest.mark.parametrize("wire1, wire2, distance, _", testdata)
+def test_answer_part1(wire1, wire2, distance, _):
     assert answer_part1(wire1, wire2) == distance
+
+
+@pytest.mark.parametrize("wire1, wire2, _, steps", testdata)
+def test_answer_part2(wire1, wire2, _, steps):
+    assert answer_part2(wire1, wire2) == steps
