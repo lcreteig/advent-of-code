bc37d45079e581aab952b0d6e383dfc0eef15135
Leon Reteig <leonreteig@gmail.com>
2021-02-14 16:40:46 +0100

    Add day 4 part 2

--- 2019/python/day04/day04.py
+++ 2019/python/day04/day04.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python3
 
 
-def check_two_adjacent(number: int):
+def check_adjacent(number: int):
     num2str = str(number)  # make it iterable
     digit1 = num2str[0]  # start with the first digit
+    group_sizes = []  # list of all groups of adjacent digits
+    grp = 1  # initialize counter for first group
     for digit2 in num2str[1:]:  # loop over all but first digit
         if digit1 == digit2:  # if the next digit is equal
-            return True  # stop
+            grp += 1
         else:
             digit1 = digit2  # shift to the next digit
-    return False
+            group_sizes.append(grp)  # we're at the end of this group; remember it
+            grp = 1
+    group_sizes.append(grp)  # also remember last group
+    return group_sizes
 
 
 def check_not_descending(number: int):
@@ -24,16 +29,27 @@ def check_not_descending(number: int):
     return True
 
 
-def n_passwords(start: int, end: int):
+def n_passwords(start: int, end: int, max_len=None):
     if (len(str(start)) != 6) or (len(str(end)) != 6):
         raise ValueError("The start and/or end of the range is not a 6 digit number")
 
     n = 0
+    # fmt: off
     for number in range(start, end + 1):
-        if check_two_adjacent(number) & check_not_descending(number):
-            n += 1
+        if max_len:  # For Part 2: check if any groups of digits of exactly max_len (2)
+            if any(
+                [x == max_len for x in check_adjacent(number)]
+            ) & check_not_descending(number):
+                n += 1
+        else:  # For Part 1: check if any groups of digits (at least two consecutive)
+            if any(
+                [x > 1 for x in check_adjacent(number)]
+            ) & check_not_descending(number):
+                n += 1
+    # fmt: on
     return n
 
 
 if __name__ == "__main__":
     print("Part 1: ", n_passwords(124075, 580769))
+    print("Part 2: ", n_passwords(124075, 580769, max_len=2))
